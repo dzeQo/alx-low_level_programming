4a066b9cf4afb2f1e6c66ae4854e7a65c80f490c
dzeQo <zaki.lbssir@gmail.com>
2023-06-15 07:53:14 -0700

    last commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (indicating an error condition)
+ */
+int main(void)
+{	
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fputs(message, stderr);
+	return (1);
+}
