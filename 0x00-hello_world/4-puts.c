9e45b8fab4227df9a2aa67a76cc872cb4f701261
dzeQo <zaki.lbssir@gmail.com>
2023-06-15 07:01:21 -0700

    t4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@ int main(void)
     // Use the function puts to print the string
     puts("Programming is like building a multilingual puzzle\n");
     // Return the value 0 to indicate success
-    return (0);
+    return 0;
 }
 
