70218d9b52bd71b35ded25f1f207f3817ab4e80f
dzeQo <zaki.lbssir@gmail.com>
2023-06-15 08:21:41 -0700

    last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
-#include <unistd.h>
-
+#include<unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 1 (indicating an error condition)
+ * Return: 1 (success)
  */
 int main(void)
 {
-char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(2, str, 59);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
 
