de79da59cc5130e1dd46ea16131b176f9f8f71b0
dzeQo <zaki.lbssir@gmail.com>
2023-06-16 09:30:42 -0700

    tt

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the sign of the number
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int nbr = 0;
+
+	while (nbr < 10)
+	{
+		putchar(nbr + '0');
+		putchar(',');
+		putchar(' ');
+		nbr++;
+	}
+	putchar('\n');
+	return (0);
+}
