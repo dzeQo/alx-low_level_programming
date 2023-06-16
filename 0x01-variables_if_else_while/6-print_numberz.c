4f8d799dc1be5494f3f0a742abc770ae68b52e07
dzeQo <zaki.lbssir@gmail.com>
2023-06-16 08:43:12 -0700

    tt

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the sign of the number
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char nbr = 0;
+
+	while (nbr < 10)
+	{
+		putchar(nbr+'0');
+		nbr++;
+	}
+	putchar('\n');
+	return (0);
+}
