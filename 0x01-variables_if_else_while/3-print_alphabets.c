5ecbb8a45e3901cd8e5cc110abdcfa31f1953059
dzeQo <zaki.lbssir@gmail.com>
2023-06-16 07:18:54 -0700

    t4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the sign of the number
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char alphabet = 'a';
+	
+	char alUp = 'A';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	 while (alUp <= 'Z')
+        {
+                putchar(alUp);
+                alUp++;
+        }
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the sign of the number
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'e' && alphabet != 'q' ){
+			putchar(alphabet);
+		}
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
