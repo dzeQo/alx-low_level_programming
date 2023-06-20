74f94906755a00088c4c6a0619057e484479719b
dzeQo <zaki.lbssir@gmail.com>
2023-06-20 09:08:18 -0700

    tt

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print the  last digits of a number
+ *
+ * @n: an integer number  
+ *
+ * Return : the lase digit
+ */
+int print_last_digit(int n)
+{
+	int m = n % 10;
+
+	if (n < 0)
+	{
+		m *= -1;
+	}
+	_putchar(m + '0');
+	return (m);
+}
