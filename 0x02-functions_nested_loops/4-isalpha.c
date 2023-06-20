2c541635148e24e502e6ea20e86b9e03e6da64cd
dzeQo <zaki.lbssir@gmail.com>
2023-06-20 07:36:00 -0700

    tt

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  * Return: Always 0.
  */
 
-int _isalpha(int c)
+int  _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
