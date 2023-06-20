d8504b17bf694e22e7ee2734afe5a852a18fe27f
dzeQo <zaki.lbssir@gmail.com>
2023-06-20 06:35:48 -0700

    t1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		int alph = 'a';
+
+		while (alph <= 'z')
+		{
+			_putchar('alph');
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
