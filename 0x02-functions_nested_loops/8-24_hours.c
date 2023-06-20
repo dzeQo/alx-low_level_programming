ab293d07b97a4c1178c6ef044d2794e7d8c33a31
dzeQo <zaki.lbssir@gmail.com>
2023-06-20 09:25:50 -0700

    tt

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60 ; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putcahr(':');
+			_putchar(j / 10 + '0');
+                        _putchar(j % 10 + '0');
+                        _putcahr('\n');
+
+		}
+	}
+}
