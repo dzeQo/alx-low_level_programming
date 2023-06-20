4c9dd3424b4a5917cd1a1f586c41a346b22e10fa
dzeQo <zaki.lbssir@gmail.com>
2023-06-20 08:43:05 -0700

    tt

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * abs - return 1 if the character is a lowercase, and 0 otherwise.
+ * _abs - return 1 if the character is a lowercase, and 0 otherwise.
  * @n: the number to be checked
  * Return: Always 0.
  */
 
-int abs(int n)
+int _abs(int n)
 {
 	if (n < 0)
 	{
