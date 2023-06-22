2d3583943b5b53f4d048edf2b414c08a49580b4e
dzeQo <zaki.lbssir@gmail.com>
2023-06-22 06:54:08 -0700

    commit changes

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers between 0 and 9 .
+ *
+ *
+ * return: 0 always success .
+ */
+void print_numbers()
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar('i');
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
