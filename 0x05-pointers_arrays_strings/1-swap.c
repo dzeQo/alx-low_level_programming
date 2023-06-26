b88d396edcd83610ac21a4c6c0f9213a3a200469
dzeQo <zaki.lbssir@gmail.com>
2023-06-26 04:13:10 -0700

    some changes

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of two  integers .
+ *
+ * @a: the first number
+ * @b: the second number
+ * @c: temporary var
+ * Return: 0 success .
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = *c;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
 pointers directory , it's a wild card!
+void swap_int(int *a, int *b);
