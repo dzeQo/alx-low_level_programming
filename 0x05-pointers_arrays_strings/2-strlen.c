85739f0e2da599b160b998db1c1d2f85196553e0
dzeQo <zaki.lbssir@gmail.com>
2023-06-26 04:36:41 -0700

    some changes

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * _strlen - return the sizeof a string
+ *
+ * @s: the string to be checked
+ *
+ * return: the result of the function
+ */
+int _strlen(char *s);
+{
+	int i = 0;
+
+	while(s[i])
+	{
+		i++;
+	}
+	return (i);
+}
