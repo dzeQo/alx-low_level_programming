cd7113e9e7194878c4ac0cfb634b7e721eda9d1f
dzeQo <zaki.lbssir@gmail.com>
2023-06-26 05:36:51 -0700

    some changes

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * print_rev - print the string revesed
+ *
+ * @s: pointer ro string
+ *
+ * return: the reversed string
+ */
+
+void print_rev(char *c)
+{
+	int len = 0;
+	int i;
+
+	while (s[len])
+	{
+		len++;
+	}
+	for (i = len-1; i <= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
