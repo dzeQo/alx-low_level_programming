0c41e710ea92ab770393d9c34c16ac88c620f04e
dzeQo <zaki.lbssir@gmail.com>
2023-06-26 05:54:01 -0700

    some changes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
 /*
  * print_rev - print the string revesed
  *
- * @s: pointer ro string
+ * @c: pointer ro string
  *
  * return: the reversed string
  */
@@ -13,13 +13,13 @@ void print_rev(char *c)
 	int len = 0;
 	int i;
 
-	while (s[len])
+	while (c[len])
 	{
 		len++;
 	}
 	for (i = len-1; i <= 0; i++)
 	{
-		_putchar(s[i]);
+		_putchar(c[i]);
 	}
 	_putchar('\n');
 }
