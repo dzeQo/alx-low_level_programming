84697589d3efc5727e953c7084683987253bdc84
dzeQo <zaki.lbssir@gmail.com>
2023-07-03 08:53:02 -0700

    changes

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
