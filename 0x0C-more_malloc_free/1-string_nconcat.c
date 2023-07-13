d34274dd05f968dc3c73288c8d03c9aa2e630c58
dzeQo <zaki.lbssir@gmail.com>
2023-07-13 13:01:06 -0700

    changes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,43 +1,98 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
-
+char *_strncat(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src);
 /**
- * string_nconcat - Concatenates two strings up to n bytes
- * @s1: First string
- * @s2: Second string
- * @n: Maximum number of bytes from s2 to concatenate
- * Return: Pointer to the concatenated string, or NULL on failure
+ * string_nconcat - function that concatenates two strings.
+ * @s1: ptr to a str.
+ * @s2: ptr to str.
+ * @n: num of bytes concatenated from s2.
+ * Return: str or NULL.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
 	char *str;
-	unsigned int len = n + strlen(s1);
+	int len;
 
-	if (!s1)
+	if (s1 == NULL)
 		s1 = "";
-	else if (!s2)
+
+	if (s2 == NULL)
 		s2 = "";
 
-	str = (char *)malloc(len);
-	if (!str)
-		return (NULL);
+	len = _strlen(s1) + n + 1;
 
-	for (i = 0; i < len; i++)
+	str = malloc(len);
+	if (str == NULL)
 	{
-		if (i < strlen(s1))
-		{
-			str[i] = s1[i];
-		} else
-		{
-			str[i] = *(s2++);
-		}
+		return (NULL);
 	}
 
-	str[len] = '\0';
+	_strcpy(str, s1);
+	_strncat(str, s2, n);
 
 	return (str);
+}
+/**
+ *  _strlen -  calculates the length of a string.
+ * @s: pointer to a string.
+ * Return: lenght.
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+	return (lenght);
+}
+/**
+ * _strcpy - copys str to another
+ * @dest: destination string
+ * @src: the str that we gonna copy
+ *
+ * Return: the pointer to dst
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strncat - concatenate two strings
+ *
+ * @dest: destination string
+ * @src: source srting
+ * @n: integer that specify the max num of chars copy from src to dest
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
