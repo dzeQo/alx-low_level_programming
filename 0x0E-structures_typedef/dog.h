4fdaf6ecbda80cf5935db8d9878d6621999bb051
dzeQo <zaki.lbssir@gmail.com>
2023-07-17 04:15:46 -0700

    some changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #include<stdio.h>
+#ifndef MYFILE_H
+#define MYFILE_H
+/**
+ * struct dog - has 3 elements.
+ * @name: 1st element.
+ * @age: 2nd element.
+ * @owner: 3rd element.
+ */
 struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
-}
+};
