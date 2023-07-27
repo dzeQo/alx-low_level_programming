dc69fdf14300967889f601c63c1690ca1ecf5c8f
dzeQo <zaki.lbssir@gmail.com>
2023-07-27 04:48:02 -0700

    last commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,31 +1,20 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: pointer to the first node.
- * Return: the number of nodes.
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: pointer to the first node of the list.
+ * Return: number of elements in the list.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-	char *str;
-	unsigned int len;
 	size_t count = 0;
 
 	while (h)
 	{
-		len = h->len;
-		str = h->str;
-		if (str)
-		{
-			printf("[%u] %s\n", len, str);
-		}
-		else
-		{
-			printf("[0] (nil)\n");
-		}
-		h = h->next;
 		count++;
+		h = h->next;
 	}
+
 	return (count);
 }
 
