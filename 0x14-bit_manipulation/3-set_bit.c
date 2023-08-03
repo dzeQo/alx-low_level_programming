31ae8880894da1dce00c5ef0f4c628a3fa5c876d
dzeQo <zaki.lbssir@gmail.com>
2023-08-03 03:41:15 -0700

    mendatory part 2

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,15 +1,17 @@
 #include "main.h"
-
 /**
- * set_bit - set the value of the given bit to 1 at the given index.
+ * set_bit -  sets the value of a bit to 1 at a given index.
  * @n: ptr to the int
- * @i: the index starting from 0.
- * Return: 1 or -1.
- */
-int sit_bit(unsigned long int *n, unsigned int i)
+ * @index: index starting from 0 of the bit you want to set
+ * Return: 1 or -1
+*/
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (!n || i >= sizeof(unsigned long int) * 8)
+
+	if (!n || index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	*n |= (1UL << i);
+
+	*n |= (1UL << index);
+
 	return (1);
 }
